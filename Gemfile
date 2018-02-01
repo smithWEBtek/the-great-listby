b0f2d0033483efc8be372aef9938f52e3d14bf15
americanmouths <shiverr@gmail.com>
2018-01-31 19:09:40 -0800

    Initial setting up of Omniauth and Devise

--- Gemfile
+++ Gemfile
@@ -29,6 +29,11 @@ gem 'jbuilder', '~> 2.5'
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
+gem 'devise'
+gem 'omniauth'
+gem 'omniauth-facebook'
+gem 'omniauth-github'
+gem 'dotenv-rails'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'welcome#home'
   resources :reviews
   resources :books
   resources :book_lists
+
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
