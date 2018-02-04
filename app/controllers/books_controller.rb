205fc52f976cc856308084bf7d0ee49fd7273f0f
americanmouths <shiverr@gmail.com>
2018-02-04 14:09:08 -0800

    Create a book, needs refactoring

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,9 +3,26 @@ class BooksController < ApplicationController
     @books = Book.all
   end
 
-  def new 
+  def new
+    @book = Book.new
   end
 
   def create
+    @book = Book.new
+    @book.title = params[:book][:title]
+    @book.blurb = params[:book][:blurb]
+    @book.genre = Genre.find_or_create_by(name: params[:book][:genre])
+    @book.author = Author.find_or_create_by(name: params[:book][:author])
+    @book.save
+    redirect_to book_path(@book)
   end
+
+  def show
+    @book = Book.find_by(id: params[:id])
+  end
+
+  private
+    def book_params
+      params.require(:book).permit(:title, :blurb, :date_started, :date_completed, :genre_name)
+    end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -6,4 +6,13 @@ class Book < ApplicationRecord
   has_many :reviews
   has_many :users, :through => :reviews
 
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    binding.pry
+    self.genre.name
+  end
+
 end
