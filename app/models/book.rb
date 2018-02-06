93e06fd9c2aa6f622b5cfc5fa80a38ecfd0a6a13
americanmouths <shiverr@gmail.com>
2018-02-05 21:12:59 -0800

    update custom attribute writer to include status of book

--- app/models/book.rb
+++ app/models/book.rb
@@ -26,7 +26,7 @@ class Book < ApplicationRecord
   def book_lists_attributes=(book_list_attributes)
     book_list_attributes.values.each do |book_list_attribute|
       user = User.find_by(id: book_list_attribute[:user_id])
-      book_list = BookList.find_or_create_by(title: book_list_attribute[:title], user: user)
+      book_list = BookList.find_or_create_by(title: book_list_attribute[:title], user: user, status: book_list_attribute[:status])
       self.book_lists.build(title: book_list.title, user_id: book_list.user_id)
       end
     end
