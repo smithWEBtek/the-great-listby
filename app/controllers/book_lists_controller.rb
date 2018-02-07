c4affe8b289d2ad80f8c88b1c4a6519c187207d4
americanmouths <shiverr@gmail.com>
2018-02-06 20:18:01 -0800

    Add books to book lists, including nested attributes and book_ids= methods

--- app/controllers/book_lists_controller.rb
+++ app/controllers/book_lists_controller.rb
@@ -6,9 +6,11 @@ class BookListsController < ApplicationController
 
   def new
     @booklist = BookList.new
+    @user = current_user
   end
 
   def create
+    @user = current_user
     @booklist = BookList.create(book_list_params)
     if @booklist.save
       redirect_to book_list_path(@booklist)
@@ -17,9 +19,13 @@ class BookListsController < ApplicationController
     end
   end
 
+  def show
+    @booklist = BookList.find_by(id: params[:id])
+  end
+
   private
    def book_list_params
-     params.require(:book_list).permit(:title, :book_ids => [])
+     params.require(:book_list).permit(:title, :user_id, :book_ids => [], :books_attributes => [:title, :genre, :author])
    end
 
 end
--- app/models/book_list.rb
+++ app/models/book_list.rb
@@ -3,4 +3,26 @@ class BookList < ApplicationRecord
   has_many :book_lists_books
   has_many :books, :through => :book_lists_books
 
+  def books_attributes=(book_attributes)
+    book_attributes.values.each do |book_attribute|
+    if book_attribute[:title].present? && book_attribute[:genre].present? && book_attribute[:author].present?
+        genre = Genre.find_or_create_by(name: book_attribute[:genre])
+        author = Author.find_or_create_by(name: book_attribute[:author])
+        book = Book.find_or_create_by(title: book_attribute[:title], genre: genre, author: author)
+          if !self.books.include?(book)
+            self.books.build(title: book.title, genre: book.genre, author: book.author)
+          end
+        end
+      end
+    end
+
+  def book_ids=(id)
+    id.each do |i|
+    book = Book.find_by(id: i)
+      if book
+        self.books << book
+      end
+    end
+  end
+
 end
