0a4a29509972d22aa240c90fac530e617acd9cfc
americanmouths <shiverr@gmail.com>
2018-02-06 18:44:06 -0800

    Remove new/create paths

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -4,29 +4,8 @@ class GenresController < ApplicationController
     @genres = Genre.all
   end
 
-  def new
-    @genre = Genre.new
-    3.times do
-      @genre.books.build
-    end
-  end
-
-  def create
-    @genre = Genre.create(genre_params)
-    if @genre.save
-      redirect_to genre_path(@genre)
-    else
-      render :new
-    end
-  end
-
   def show
     @genre = Genre.find_by(id: params[:id])
   end
-
-  private
-    def genre_params
-      params.require(:genre).permit(:name, :book_ids => [], books_attributes: [:title, :author])
-    end
 
 end
