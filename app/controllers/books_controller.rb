96c0b155964e29c7e57f0071ec28056fbc4ee908
americanmouths <shiverr@gmail.com>
2018-02-10 12:18:10 -0800

    Refactor create method to select join table and add book to booklist upon creation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,7 @@ class BooksController < ApplicationController
 
   def new
     @book = Book.new
+    @booklist = BookList.find_by(id: params[:book_list_id])
     @user = current_user
     @book.book_features.build
   end
@@ -12,9 +13,10 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
+      @booklist = BookList.find_by(id: params[:book][:book_list_id])
       @book_features = BookFeature.find_by(id: @book.book_features.ids)
       @book_features.update_status(params[:book][:book_features])
-      redirect_to book_path(@book)
+      redirect_to book_list_path(@booklist)
     else
       render :new
     end
@@ -24,14 +26,20 @@ class BooksController < ApplicationController
     @book = Book.find_by(id: params[:id])
   end
 
+  def edit
+    @book = Book.find_by(id: params[:id])
+    @booklist = BookList.find_by(id: params[:id])
+  end
+
+  def update
+  end
+
   private
     def book_params
-      params.require(:book).permit(:title,
+      params.require(:book).permit(:book_list_id, :title,
         :blurb,
         :genre_name,
         :author_name,
-        :book_list_ids => [],
-        book_lists_attributes: [:title, :user_id],
         :book_features => [])
     end
 end
