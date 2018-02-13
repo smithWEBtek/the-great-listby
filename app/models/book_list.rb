c6be141f643b8641aab254b9b1e4b0f17bbdc539
americanmouths <shiverr@gmail.com>
2018-02-12 20:52:26 -0800

    fix nested attributes method to not create duplicate books

--- app/models/book_list.rb
+++ app/models/book_list.rb
@@ -3,15 +3,15 @@ class BookList < ApplicationRecord
   has_many :book_features
   has_many :books, :through => :book_features
   validates :title, :presence => true
-  
+
   def books_attributes=(book_attributes)
     book_attributes.values.each do |book_attribute|
     if book_attribute[:title].present? && book_attribute[:genre].present? && book_attribute[:author].present?
         genre = Genre.find_or_create_by(name: book_attribute[:genre])
         author = Author.find_or_create_by(name: book_attribute[:author])
-        book = Book.find_or_create_by(title: book_attribute[:title], genre: genre, author: author)
+        book = Book.find_or_create_by(title: book_attribute[:title], genre: genre, author: author, blurb: book_attributes[:blurb])
           if !self.books.include?(book)
-            self.books.build(title: book.title, genre: book.genre, author: book.author)
+            self.books << book
           end
         end
       end
@@ -27,5 +27,4 @@ class BookList < ApplicationRecord
       end
     end
   end
-
 end
