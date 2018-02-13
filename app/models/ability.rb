7435f4842aab2832e926fe3ef2ce5f23c2c66b45
americanmouths <shiverr@gmail.com>
2018-02-12 18:03:15 -0800

    Update cancan abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,20 +2,20 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :read, Review
-    can :read, Book
-    can :read, Author
-    can :read, Genre
-
-    unless user.nil?
-      can :manage, BookList, { user_id: user.id }
-      can :manage, Review, { user_id: user.id }
-      can :create, Book
-      can :update, Book
-    end
-
-    if user.admin?
-      can :mange, :all
+    if user.present?
+      if user.admin?
+        can :manage, :all
+      else
+        can :manage, :book_lists, { user_id: user.id }
+        can :manage, :reviews, { user_id: user.id }
+        can :create, :books
+        can :update, :books
+      end
+    else
+      can :read, :reviews
+      can :read, :books
+      can :read, :authors
+      can :read, :genres
     end
   end
 end
