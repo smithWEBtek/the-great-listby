6888dbdeafbb5b1d6a10f1dde99efce94249345c
americanmouths <shiverr@gmail.com>
2018-02-13 11:57:03 -0800

    remove unnecessary self.titles method

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,16 +5,10 @@ class Book < ApplicationRecord
   has_many :book_lists, :through => :book_features
   has_many :reviews
   has_many :users, :through => :reviews
-
   validates :title, presence: true
-
   scope :highest_rated_books, -> { joins(:reviews).where('reviews.rating > 3') }
   scope :lowest_rated_books, -> { joins(:reviews).where('reviews.rating <= 3') }
   scope :by_user, ->(user) { joins(:reviews).where('reviews.user_id = ?', user.id ) }
-
-  def self.titles
-    pluck(:title)
-  end
 
   def genre_name=(name)
     self.genre = Genre.find_or_create_by(name: name)
