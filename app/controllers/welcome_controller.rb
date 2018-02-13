0c68118a421726c1fc0aaa949ffdb6b613c77b4b
americanmouths <shiverr@gmail.com>
2018-02-13 10:40:22 -0800

    change home route to set @user to current_user correctly

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,13 +1,7 @@
 class WelcomeController < ApplicationController
 
   def home
-    if current_user
-      @user = current_user
-    else
-      @user = User.find_by_id(session[:user_id])
-    end
-      @user.save if @user
-      current_user = @user
+    @user = current_user
   end
 
 end
