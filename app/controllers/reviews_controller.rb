73def51e16e851a0fda6f8d7215795958b844d4d
americanmouths <shiverr@gmail.com>
2018-02-13 11:21:36 -0800

    add cancan abilities, create set_current_user method, create find_review_by_book for dry reasons

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,14 @@
 class ReviewsController < ApplicationController
+  before_action :set_current_user
 
   def index
     @book = Book.find_by(id: params[:book_id])
   end
 
   def new
+    authorize! :create, Review
     @review = Review.new
     @book = Book.find_by(id: params[:book_id])
-    @user = current_user
   end
 
   def create
@@ -20,28 +21,23 @@ class ReviewsController < ApplicationController
   end
 
   def show
-    @review = Review.find_by(id: params[:id])
-    @book = Book.find_by(id: params[:book_id])
-    @user = current_user
-    authorize! :read, @review
+    find_review_by_book
   end
 
   def edit
-    @book = Book.find_by(id: params[:book_id])
-    @review = Review.find_by(id: params[:id])
-    @user = current_user
+    find_review_by_book
   end
 
   def update
-    @book = Book.find_by(id: params[:book_id])
-    @review = Review.find_by(id: params[:id])
+    find_review_by_book
+    authorize! :update, @review
     @review.update(review_params)
     redirect_to book_review_path(@book, @review)
   end
 
   def destroy
-    @book = Book.find_by(id: params[:book_id])
-    @review = Review.find_by(id: params[:id])
+    find_review_by_book
+    authorize! :destroy, @review
     @review.destroy
     redirect_to book_path(@book)
   end
@@ -50,4 +46,14 @@ class ReviewsController < ApplicationController
     def review_params
       params.require(:review).permit(:title, :content, :book_id, :user_id, :rating)
     end
+
+    def set_current_user
+      @user = current_user
+    end
+
+    def find_review_by_book
+      @review = Review.find_by(id: params[:id])
+      @book = Book.find_by(id: params[:book_id])
+    end
+
 end
