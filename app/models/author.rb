4bd2c67bdf649ddd9f83998b8cd3fb7f5eeeea61
americanmouths <shiverr@gmail.com>
2018-02-20 18:08:43 -0800

    Remove unnecessary has_many through declarations

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,7 +1,5 @@
 class Author < ApplicationRecord
   has_many :books
-  has_many :genres, :through => :books
   validates :name, presence: true
-
   scope :alphabatize, -> { order('name asc') }
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,6 +1,5 @@
 class Genre < ApplicationRecord
   has_many :books
-  has_many :authors, :through => :books
   validates :name, presence: true
   scope :alphabatize, -> { order('name asc') }
 end
