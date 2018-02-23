d78c2d6dd08daa9c022cfa47e7da14b71841c489
americanmouths <shiverr@gmail.com>
2018-02-22 18:16:28 -0800

    add custom url helper methods, update header to dynamically include new url helpers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'welcome#home'
-  get '/users/:id/reviews' => "users#reviews"
-  get '/users/:id/highest_rated_books' => "users#highest_rated_books"
-  get '/users/:id/lowest_rated_books' => "users#lowest_rated_books"
+  get '/users/:id/reviews', to: 'users#reviews', as: 'users_reviews'
+  get '/users/:id/highest_rated_books', to: 'users#highest_rated_books', as: 'users_highest_rated_books'
+  get '/users/:id/lowest_rated_books', to: 'users#lowest_rated_books', as: 'users_lowest_rated_books'
 
   resources :books do
     resources :reviews
